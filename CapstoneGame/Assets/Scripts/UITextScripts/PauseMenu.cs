13f8273b5a2dda729e50fe4e410a1a2b9551af6d
Lermonz <cgntwo@gmail.com>
2025-02-14 20:18:09 -0500

    imported samples eugh....

--- CapstoneGame/Assets/Scripts/UITextScripts/PauseMenu.cs
+++ CapstoneGame/Assets/Scripts/UITextScripts/PauseMenu.cs
@@ -4,11 +4,15 @@ using UnityEngine.EventSystems;
 public class PauseMenu : MonoBehaviour
 {
     public static bool _isPaused = false;
+    [Header ("Panels")]
     public GameObject _pauseMenu;
     public GameObject _settingsMenu;
+    public GameObject _controlsMenu;
 
+    [Header ("Event System wants a first selected button")]
     [SerializeField] private GameObject _mainFirstButton;
     [SerializeField] private GameObject _settingsFirstButton;
+    [SerializeField] private GameObject _controlsFirstButton;
 
     private void Update() {
         if(InputManager.Instance.MenuOpenCloseInput) {
@@ -31,20 +35,29 @@ public class PauseMenu : MonoBehaviour
     private void CloseMenus() {
         _pauseMenu.SetActive(false);
         _settingsMenu.SetActive(false);
+        _controlsMenu.SetActive(false);
         EventSystem.current.SetSelectedGameObject(null);
     }
     private void OpenMainPauseMenu() {
+        CloseMenus();
         _pauseMenu.SetActive(true);
-        _settingsMenu.SetActive(false);
         EventSystem.current.SetSelectedGameObject(_mainFirstButton);
     }
     private void OpenSettingsMenu() {
-        _pauseMenu.SetActive(false);
+        CloseMenus();
         _settingsMenu.SetActive(true);
         EventSystem.current.SetSelectedGameObject(_settingsFirstButton);
     }
+    private void OpenControlsMenu() {
+        CloseMenus();
+        _controlsMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(_controlsFirstButton);
+    }
     public void OnSettingsPress() {
         OpenSettingsMenu();
+    }
+    public void OnControlsPress() {
+        OpenControlsMenu();
     }
     public void OnBackPress() {
         OpenMainPauseMenu();
