dd2b4132403f2942d707ffb24191efc02d891e9f
Lermonz <cgntwo@gmail.com>
2025-06-17 15:48:36 +0200

    destructable blocks functionality

--- CapstoneGame/Assets/Scripts/InteractablesBehaviours/DestructableBlockBehaviour.cs
+++ CapstoneGame/Assets/Scripts/InteractablesBehaviours/DestructableBlockBehaviour.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class DestructableBlockBehaviour : MonoBehaviour
 {
-    void OnCollisionExit2D(Collision2D collision)
+    public bool _beenTouched;
+    public void BeenTouched()
     {
-        Debug.Log("Fragile left");     
-        Destroy(this.gameObject, 0.2f);
+        _beenTouched = true;
+        Debug.Log("Fragile left");
+        this.gameObject.GetComponent<SpriteRenderer>().color *= 0.75f;
+        Destroy(this.gameObject, 0.9f);
+    }
+    void Update()
+    {
+        if (_beenTouched)
+        {
+            this.transform.position -= Vector3.up * 0.25f * Time.deltaTime;
+        }
     }
 }
--- CapstoneGame/Assets/Scripts/Player/Controller2D.cs
+++ CapstoneGame/Assets/Scripts/Player/Controller2D.cs
@@ -25,6 +25,8 @@ public class Controller2D : MonoBehaviour
     public bool _hitCeiling;
     public bool _hitWall;
     public bool _canDoubleJump;
+    bool _destructable;
+    DestructableBlockBehaviour _destructableBlock;
 
     float _vertMult = 0.95f;
 
@@ -55,15 +57,17 @@ public class Controller2D : MonoBehaviour
         Vector2 dir = new Vector2(goal.x-this.transform.position.x,goal.y-this.transform.position.y);
         transform.Translate(dir*str);
     }
-    void VertCollisions(ref Vector2 velocity) {
+    void VertCollisions(ref Vector2 velocity)
+    {
         float directionY = Mathf.Sign(velocity.y);
-        float rayLength = Mathf.Abs(velocity.y)+boundInset;
-
+        float rayLength = Mathf.Abs(velocity.y) + boundInset;
+        _destructable = false;
         _vertElseCount = 0;
 
-        for(int i = 0; i < _vertRayCount; i++) {
+        for (int i = 0; i < _vertRayCount; i++)
+        {
             float _tempVertRaySpacing = directionY == -1 ? _vertRaySpacing * 0.9f : _vertRaySpacing * 0.7f;
-            Vector2 rayOrigin = directionY == -1 ? _raycastOrigins.botleft + Vector2.right*0.05f : _raycastOrigins.topleft + Vector2.right*0.15f;
+            Vector2 rayOrigin = directionY == -1 ? _raycastOrigins.botleft + Vector2.right * 0.05f : _raycastOrigins.topleft + Vector2.right * 0.15f;
             rayOrigin += Vector2.right * i * _tempVertRaySpacing;
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collMask);
             Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
@@ -85,6 +89,11 @@ public class Controller2D : MonoBehaviour
                     _isGrounded = true;
                     _vcamTransposer.m_DeadZoneHeight = 0;
                 }
+                if (hit.collider.CompareTag("DestructableBlock"))
+                {
+                    _destructable = true;
+                    _destructableBlock = hit.collider.GetComponent<DestructableBlockBehaviour>();
+                }
 
             }
             else
@@ -93,44 +102,64 @@ public class Controller2D : MonoBehaviour
                 _vcamTransposer.m_DeadZoneHeight = 0.2f;
             }
         }
-        if(_vertElseCount >= _vertRayCount) {
+        if (_vertElseCount >= _vertRayCount)
+        {
             _isGrounded = false;
             _hitCeiling = false;
+            _destructable = false;
         }
-        if(_hitWall && _vertElseCount > 0) {
+        if (_hitWall && _vertElseCount > 0)
+        {
             _isGrounded = false;
         }
+        if (_destructable)
+        {
+            TouchedDestructableBlock(ref _destructableBlock);
+        }
     }
-    void HorzCollisions(ref Vector2 velocity) {
+    void HorzCollisions(ref Vector2 velocity)
+    {
         float directionX = Mathf.Sign(velocity.x);
-        float rayLength = Mathf.Abs(velocity.x)+boundInset;
+        float rayLength = Mathf.Abs(velocity.x) + boundInset;
         _horzElseCount = 0;
+        _destructable = false;
 
-        for(int i = 1; i < _vertRayCount; i++) {
+        for (int i = 1; i < _vertRayCount; i++)
+        {
             Vector2 rayOrigin = directionX == -1 ? _raycastOrigins.botleft : _raycastOrigins.botright;
             rayOrigin += Vector2.up * (_horzRaySpacing * _vertMult * i);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collMask);
             Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
 
-            if(hit) {
-                if (hit.distance == 0)
-                {
-                    GetOutOfWallCollision(ref velocity);
-                }
-                else
-                {
-                    velocity.x = (hit.distance-boundInset) * directionX;
-                }
+            if (hit)
+            {
+                velocity.x = (hit.distance - boundInset) * directionX;
                 //velocity.y = Mathf.Clamp(velocity.y, -Mathf.Infinity, 0);
                 rayLength = hit.distance;
                 _hitWall = true;
+                if (hit.collider.CompareTag("DestructableBlock"))
+                {
+                    _destructable = true;
+                    _destructableBlock = hit.collider.GetComponent<DestructableBlockBehaviour>();
+                }
             }
-            else {
+            else
+            {
                 _horzElseCount++;
             }
         }
-        if(_horzElseCount >= _horzRayCount-2) {
+        if (_horzElseCount >= _horzRayCount - 2)
+        {
             _hitWall = false;
+            _destructable = false;
+        }
+        if (_horzElseCount == 0)
+        {
+            GetOutOfWallCollision(ref velocity);
+        }
+        if (_hitWall && _destructable)
+        {
+            TouchedDestructableBlock(ref _destructableBlock);
         }
 
     }
@@ -141,10 +170,10 @@ public class Controller2D : MonoBehaviour
         Vector2 rayOrigin = directionX == 1 ? _raycastOrigins.botleft + Vector2.right * 0.25f : _raycastOrigins.botright - Vector2.right * 0.25f;
         RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collMask);
         Vector2 rayOrigin2 = directionX == 1 ? _raycastOrigins.topleft + Vector2.right * 0.25f : _raycastOrigins.topright - Vector2.right * 0.25f;
-        RaycastHit2D hit2 = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collMask);
+        RaycastHit2D hit2 = Physics2D.Raycast(rayOrigin2, Vector2.right * directionX, rayLength, collMask);
         if (hit && hit2)
         {
-            if (hit.distance == 0)
+            if (hit.distance == 0 && hit2.distance == 0)
             {
                 GetOutOfWallCollision(ref velocity);
             }
@@ -152,6 +181,13 @@ public class Controller2D : MonoBehaviour
             {
                 velocity.x = (hit.distance + 0.25f) * directionX;
             }
+        }
+    }
+    void TouchedDestructableBlock(ref DestructableBlockBehaviour block)
+    {
+        if (!block._beenTouched)
+        {
+            block.BeenTouched();
         }
     }
     void GetOutOfWallCollision(ref Vector2 velocity)
